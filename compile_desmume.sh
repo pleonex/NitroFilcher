f8ba06c619674e21499ba433b04ebce2784c7fcc
Benito Palacios Sánchez <benito356@gmail.com>
2016-02-29 14:15:06 +0100

    Accept the patch file from argument

--- compile_desmume.sh
+++ compile_desmume.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [[ -z "$1" ]] ; then
+    echo "The first argument must be the relative path to the patch to apply."
+    exit -1
+fi
+
 if [[ ! -d "desmume" ]] ; then
     # Checkout the desmume code if it doesn't exist
     svn checkout svn://svn.code.sf.net/p/desmume/code/trunk desmume
@@ -11,7 +16,7 @@ else
 fi
 
 # Apply our patch
-patch -p0 -i ../patch.diff
+patch -p0 -i ../"$1"
 
 # Run the pre-steps
 # Yes, there is a desmume folder inside the root folder
@@ -22,10 +27,10 @@ cd desmume
 ./configure --enable-debug --enable-developer
 
 # Make
-if [[ -z "$1" ]] ; then
+if [[ -z "$2" ]] ; then
     make
 else
-    make -j$1
+    make -j$2
 fi
 
 # We are not install but copying the executable
